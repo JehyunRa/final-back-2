779f63f79a553ebd8bd36f115567b805bdec21e1
JehyunRa <jra4862@gmail.com>
2021-11-06 15:58:54 -0400

    changed db/index.js

--- src/db/index.js
+++ src/db/index.js
@@ -1,13 +1,37 @@
-const pg = require("pg");
+// const pg = require("pg");
 
-const client = new pg.Client({
-  connectionString: process.env.DATABASE_URL || ""
-});
+// const client = new pg.Client({
+//   connectionString: process.env.DATABASE_URL || ""
+// });
 
-client
-  .connect(
-    console.log(`database_URL: ${process.env.DATABASE_URL}`)
-    )
-  .catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
+// client
+//   .connect(
+//     console.log(`database_URL: ${process.env.DATABASE_URL}`)
+//     )
+//   .catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
+
+// module.exports = client;
+
+const { Pool, Client } = require('pg')
+const connectionString = process.env.DATABASE_URL || ""
+
+const pool = new Pool({
+  connectionString,
+})
+
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  pool.end()
+})
+
+const client = new Client({
+  connectionString,
+})
+client.connect()
+
+client.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  client.end()
+})
 
 module.exports = client;
