662e9b8b4d55c66ee2d429c76388c24da0ed45fb
JehyunRa <jra4862@gmail.com>
2021-11-06 16:38:35 -0400

    changed src/application.js

--- src/application.js
+++ src/application.js
@@ -51,25 +51,27 @@ module.exports = function application(
   app.use("/api", budget(db));
   app.use("/api", account(db));
 
-  if (ENV === "development" || ENV === "production") {
+  // if (ENV === "development" || ENV === "production") {
     Promise.all([
       read(path.resolve(__dirname, `db/schema/create.sql`)),
       read(path.resolve(__dirname, `db/schema/${ENV}.sql`)),
     ])
       .then(([create, seed]) => {
-        app.get("/api/debug/reset", (request, response) => {
-          db.query(create)
-            .then(() => db.query(seed))
-            .then(() => {
-              console.log("Database Reset");
-              response.status(200).send("Database Reset");
-            });
-        });
+        if (ENV === "development") {
+          app.get("/api/debug/reset", (request, response) => {
+            db.query(create)
+              .then(() => db.query(seed))
+              .then(() => {
+                console.log("Database Reset");
+                response.status(200).send("Database Reset");
+              });
+          });
+        };
       })
       .catch((error) => {
         console.log(`Error setting up the reset route: ${error}`);
       });
-  }
+  // }
 
   app.close = function () {
     return db.end();
